ce42770194c48c70d0ec892e467a62cd17a112ef
KsGr3g0ry <gbrothers29@gmail.com>
2021-04-08 14:40:54 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Form, Navbar, Nav, FormControl } from 'react-bootstrap';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <div>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">GMDB</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#login">Login</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar>
+        </div>
+        <h2>Login</h2>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control type="email"/>
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control type="password"/>
+          </Form.Group>
+          <Button color="primary">Submit</Button>
+        </Form>
     </div>
-  );
+    )
+  }
 }
+  
 
 export default App;
